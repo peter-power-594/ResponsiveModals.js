e843d1d580495ab5944a12052f1a26eeb3881f0d
Pierre-Henri Lavigne <contact@peter202202.ninja>
2023-11-27 10:51:29 +0900

    WIP: First doc released

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,6 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		sass: {
-			options: {
-				style: 'compressed',
-				noSourceMap: true
-			},
-			dist: {
-				files: {
-				  'dist/css/responsivemodals.min.css': 'src/sass/responsivemodals.scss'
-				}
-			}
-		},
-		jshint: {
-			all: [
-				'src/js/responsivemodals.js'
-			]
-		},
-		uglify: {
-			options: {
-				output: {
-					comments: 'some'
-				}
-			},
-			build: {
-				src: 'src/js/responsivemodals.js',
-				dest: 'dist/js/responsivemodals.min.js'
-			}
-		},
-		concat: {
-			build: {
-				src: [
-					'src/js/responsivemodals.js'
-				],
-				dest: 'dist/js/responsivemodals.js'
-			}
-		},
 		connect: {
 			server: {
 				options: {
@@ -51,45 +16,17 @@ module.exports = function(grunt) {
 					livereload: 35780
 				},
 				files: [
-					'src/sass/*.scss',
-					'src/js/*.js'
-				],
-				tasks: [ 'sass', 'jshint', 'uglify', 'concat' ]
-			},
-			debug: {
-				options: {
-					livereload: 35780
-				},
-				files: [
-					'src/sass/*.scss',
-					'src/js/*.js'
-				],
-				tasks: [ 'sass', 'jshint', 'concat' ]
-			},
-			js: {
-				options: {
-					livereload: 35780
-				},
-				files: [
-					'src/sass/*.scss',
-					'src/js/*.js'
-				],
-				tasks: [ 'jshint', 'concat' ]
+					'*.html',
+					'css/*.css',
+					'js/*.js'
+				]
 			}
 		}
 	});
 
-	grunt.loadNpmTasks( 'grunt-contrib-sass' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
 	grunt.registerTask( 'default', [ 'connect', 'watch:all' ] );
-
-	grunt.registerTask( 'debug', [ 'connect', 'watch:debug' ] );
-
-	grunt.registerTask( 'js', [ 'connect', 'watch:js' ] );
 
 };
